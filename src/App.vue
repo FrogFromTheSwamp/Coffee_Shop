18e8a1ac7725c3dbf97e2e3a96f0945b5e33e459
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-11-07 20:49:42 +0300

    Создан проект на Vue.js и разработаны компоненты для первой страницы

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <hero-view />
+    <footer-component />
+  </div>
+</template>
+
+<script>
+import HeroView from '@/views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: { HeroView, FooterComponent }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
