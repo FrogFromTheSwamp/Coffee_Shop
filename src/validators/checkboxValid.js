f3605335b20d4be68e09e17b6d8da723576d4e50
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-11-19 15:45:28 +0300

    Добавлена валидация формы с помощью библиотеки Vuelidate, добавлены кастомные валидаторы

---
+++ src/validators/checkboxValid.js
@@ -0,0 +1,3 @@
+export const checkboxValid = (value) => {
+    return value === true
+}
--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -7,9 +7,7 @@
                         <nav-bar-component />
                     </div>
                 </div>
-                <title-component 
-                :title="'Contact us'"
-                />
+                <title-component :title="'Contact us'" />
             </div>
         </div>
         <section class="contacts">
@@ -18,71 +16,91 @@
                     <div class="col col-12 col-lg-6 offset-0 offset-lg-3">
                         <div class="title mt-5">Tell us about your tastes</div>
                         <img class="beanslogo mt-5" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
-    
+
                         <form @submit.prevent="submit" action="#" class="mt-5">
                             <div class="form-group row">
-                                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                                <div class="col col-12 col-sm-3 d-flex align-items-start">
                                     <label for="name-input" class="mb-0">
                                         Name
                                         <span style="color: red;">*</span>
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input 
-                                    v-model="form.name"
-                                    type="text" 
-                                    class="form-control" 
-                                    id="name-input">
+                                    <input v-model="v$.name.$model" type="text" class="form-control" id="name-input">
+                                    <span v-for="error in v$.name.$errors" :key="error.$uid" class="text-danger small">
+                                        {{ error.$message }}
+                                    </span>
                                 </div>
                             </div>
-    
+
                             <div class="form-group row">
-                                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                                <div class="col col-12 col-sm-3 d-flex align-items-start">
                                     <label for="email-input" class="mb-0">
                                         E-mail
                                         <span style="color: red;">*</span>
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input 
-                                    v-model="form.email"
-                                    type="email" class="form-control" id="email-input">
+                                    <input v-model="v$.email.$model" type="email" class="form-control" id="email-input">
+                                    <span v-for="error in v$.email.$errors" :key="error.$uid" class="text-danger small">
+                                        {{ error.$message }}
+                                    </span>
                                 </div>
                             </div>
-    
+
                             <div class="form-group row">
-                                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                                <div class="col col-12 col-sm-3 d-flex align-items-start">
                                     <label for="phone-input" class="mb-0">
                                         Phone
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input 
-                                    v-model="form.phone"
-                                    type="tel" class="form-control" id="phone-input">
+                                    <input v-model="v$.phone.$model" type="tel" class="form-control" id="phone-input">
+                                    <span v-for="error in v$.phone.$errors" :key="error.$uid" class="text-danger small">
+                                        {{ error.$message }}
+                                    </span>
                                 </div>
                             </div>
-    
+
                             <div class="form-group row textarea">
-                                <div class="col col-12 d-flex justify-content-center">
+                                <div class="col col-12 d-flex justify-content-start">
                                     <label for="pmessage" class="mb-3 mt-3 text-center">
                                         Your message
                                         <span style="color: red;">*</span>
                                     </label>
                                 </div>
                                 <div class="col col-12">
-                                    <textarea 
-                                    v-model="form.message"
-                                    class="form-control" name="message" id="message" rows="5"
-                                        placeholder="Leave your comments here"></textarea>
+                                    <textarea v-model="v$.message.$model" class="form-control" name="message"
+                                        id="message" rows="5" placeholder="Leave your comments here"></textarea>
+                                    <span v-for="error in v$.message.$errors" :key="error.$uid" class="text-danger small">
+                                        {{ error.$message }}
+                                    </span>
                                 </div>
                             </div>
-    
+
+                            <div class="form-group row align-items-center">
+                                <div class="col col-12 col-sm-9">
+                                    <label for="agree-checkbox" class="mb-0 text-dark">
+                                        I agree with the offer agreement
+                                    </label>
+                                </div>
+                                <div class="col col-12 col-sm-3">
+                                    <div class="form-check d-flex justify-content-sm-end justify-content-start">
+                                        <input v-model="v$.agree.$model" type="checkbox" class="form-check-input"
+                                            id="agree-checkbox" style="width: 1.2em; height: 1.2em;">
+                                    </div>
+                                    <div class="mt-2">
+                                        <span v-for="error in v$.agree.$errors" :key="error.$uid"
+                                            class="text-danger small">
+                                            {{ error.$message }}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
                             <div class="row">
                                 <div class="col">
-                                    <button 
-                                    class="btn btn-outline-dark send-btn"
-                                    >Send us</button>
+                                    <button class="btn btn-outline-dark send-btn">Send us</button>
                                 </div>
                             </div>
                         </form>
@@ -97,22 +115,46 @@
 import NavBarComponent from '@/components/NavBarComponent.vue';
 import TitleComponent from '@/components/TitleComponent.vue';
 
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, maxLength } from '@vuelidate/validators'
+import { helpers } from '@vuelidate/validators'
+
+import { minLength } from '@/validators/minLength';
+import { checkboxValid } from '@/validators/checkboxValid';
+
 export default {
     components: { NavBarComponent, TitleComponent },
+    setup() {
+        return { v$: useVuelidate() }
+    },
     data() {
         return {
-            form: {
-                name: '',
-                email: '',
-                phone: '',
-                message: ''
-            }
+            name: '',
+            email: '',
+            phone: '',
+            message: '',
+            agree: false
         }
     },
-
+    validations() {
+        return {
+            name: { required },
+            email: { required, email },
+            phone: {},
+            agree: { required, checkboxValid: helpers.withMessage('Required element!', checkboxValid)},
+            message: { required, maxLength: maxLength(20), minLength: helpers.withMessage('This value min 5 characters!', minLength) }
+        }
+    },
     methods: {
-        submit() {
-            console.log(this.form)
+        async submit() {
+            const isFormCorrect = await this.v$.$validate()
+            if (!isFormCorrect) return
+            console.log({
+                name: this.name,
+                email: this.email,
+                phone: this.phone,
+                message: this.message
+            })
         }
     }
 }
